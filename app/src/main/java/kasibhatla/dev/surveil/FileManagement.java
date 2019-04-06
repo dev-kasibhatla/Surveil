cdd55c35e2b5166c803597f7bf215b5685f048dd
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-04-06 23:05:15 +0530

    Record video with preview
    There's still some anomaly with storage locations

---
+++ app/src/main/java/kasibhatla/dev/surveil/FileManagement.java
@@ -0,0 +1,62 @@
+package kasibhatla.dev.surveil;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class FileManagement extends AppCompatActivity {
+    private String photoFolder = MainActivity.photoFolder;
+    File folder;
+    String[] universalList;
+    private final String TAG = "file-management";
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    listFiles();
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_management);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    protected void listFiles(){
+        folder = new File(photoFolder);
+        if(folder.exists()){
+            File[] uniList = folder.listFiles();
+            for(File f:uniList){
+                Log.i(TAG, f+"\n");
+            }
+        }
+    }
+
+}
